385da13746ff607926772c94e721de8cd15f7842
Seth Speaks <sspeaks610@gmail.com>
2025-02-19 14:20:31 -0800

    Added some more complicated resp types and tests

--- lib/Resp.hs
+++ lib/Resp.hs
@@ -4,19 +4,30 @@
 module Resp where
 
 import Data.ByteString.Char8 qualified as B
+import Data.Map qualified as M
+import Data.Set qualified as S
 
-data RespData a where
-  RespSimpleString :: String -> RespData String
-  RespError :: String -> RespData String
-  RespInteger :: Integer -> RespData Integer
-  RespBulkString :: B.ByteString -> RespData B.ByteString
+data RespData where
+  RespSimpleString :: String -> RespData
+  RespError :: String -> RespData
+  RespInteger :: Integer -> RespData
+  RespBulkString :: B.ByteString -> RespData
+  RespArray :: [RespData] -> RespData
+  RespMap :: M.Map RespData RespData -> RespData
+  RespSet :: S.Set RespData -> RespData
+  deriving (Eq, Ord, Show)
 
 class Encodable a where
   encode :: a -> B.ByteString
 
-instance Encodable (RespData a) where
-  encode :: RespData a -> B.ByteString
+instance Encodable RespData where
+  encode :: RespData -> B.ByteString
   encode (RespSimpleString s) = B.concat ["+", B.pack s, "\r\n"]
   encode (RespError s) = B.concat ["-", B.pack s, "\r\n"]
   encode (RespInteger i) = B.concat [":", B.pack . show $ i, "\r\n"]
   encode (RespBulkString s) = B.concat ["$", B.pack . show . B.length $ s, "\r\n", s, "\r\n"]
+  encode (RespArray xs) = B.concat ["*", B.pack . show $ length xs, "\r\n", mconcat . map encode $ xs]
+  encode (RespSet s) = B.concat ["~", B.pack . show $ S.size s, "\r\n", mconcat . map encode $ S.toList s]
+  encode (RespMap m) = B.concat ["*", B.pack . show $ M.size m, "\r\n", mconcat . map encodePair $ M.toList m]
+    where
+      encodePair (k, v) = B.concat [encode k, encode v]
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,6 +5,8 @@ module Main where
 import Test.Hspec
 import Data.ByteString.Char8 qualified as B
 import Resp
+import Data.Map qualified as M
+import Data.Set qualified as S
 
 main :: IO ()
 main = hspec $ do
@@ -24,3 +26,17 @@ main = hspec $ do
     it "encodes bulk strings correctly" $ do
       encode (RespBulkString "foobar") `shouldBe` "$6\r\nfoobar\r\n"
       encode (RespBulkString "") `shouldBe` "$0\r\n\r\n"
+
+    it "encodes arrays correctly" $ do
+      encode (RespArray [RespSimpleString "foo", RespInteger 42]) `shouldBe` "*2\r\n+foo\r\n:42\r\n"
+      encode (RespArray []) `shouldBe` "*0\r\n"
+
+    it "encodes maps correctly" $ do
+      let mapData = M.fromList [(RespSimpleString "first", RespInteger 1), (RespSimpleString "second", RespBulkString "bulkString")]
+      encode (RespMap mapData) `shouldBe` "*2\r\n+first\r\n:1\r\n+second\r\n$10\r\nbulkString\r\n"
+      encode (RespMap M.empty) `shouldBe` "*0\r\n"
+    
+    it "encodes sets correctly" $ do
+      let setData = S.fromList [(RespSimpleString "Hello"), (RespInteger 5), RespBulkString ("im very long")]
+      encode (RespSet setData) `shouldBe` "~3\r\n+Hello\r\n:5\r\n$12\r\nim very long\r\n"
+      encode (RespSet S.empty) `shouldBe` "~0\r\n"
