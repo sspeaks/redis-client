ff7296a9be415fc11f473f63fcaf8b3f9b556af1
Seth Speaks <sspeaks610@gmail.com>
2025-02-19 17:33:34 -0800

    more tweaks

--- app/Main.hs
+++ app/Main.hs
@@ -9,45 +9,64 @@ import Data.Default.Class (def)
 import Data.IP (IPv4, fromIPv4w)
 import Data.Word (Word32)
 import Network.DNS
+  ( DNSError,
+    defaultResolvConf,
+    lookupA,
+    makeResolvSeed,
+    withResolver,
+  )
 import Network.Socket
+import Network.Socket.ByteString.Lazy (getContents, sendAll)
 import Network.TLS
 import Network.TLS.Extra.Cipher
+import Resp (RespData (RespArray, RespSimpleString, RespBulkString), encode, parseWith)
 import System.X509.Unix (getSystemCertificateStore)
+import Prelude hiding (getContents)
+
+maxRecvBytes :: Int
+maxRecvBytes = 4096
 
 main :: IO ()
 main = do
-  let hostname = "sspeaks-eus2euap-test.redis.cache.windows.net"
-  ip <- resolve hostname
-  let ipCorrectEndian = toNetworkByteOrder ip
+  let hostname = "localhost"
+  -- ip <- resolve hostname
+  let ipCorrectEndian = tupleToHostAddress (127, 0, 0, 1)
   print $ hostAddressToTuple ipCorrectEndian
-  let addrInfo = AddrInfo {addrFlags = [], addrFamily = AF_INET, addrSocketType = Stream, addrProtocol = 0, addrAddress = SockAddrInet 6380 ipCorrectEndian, addrCanonName = Just hostname}
+  let addrInfo = AddrInfo {addrFlags = [], addrFamily = AF_INET, addrSocketType = Stream, addrProtocol = 0, addrAddress = SockAddrInet 6379 ipCorrectEndian, addrCanonName = Just hostname}
   sock <- openSocket addrInfo
   connect sock (addrAddress addrInfo)
   print "Socket connected"
-
+  sendAll sock "HELLO 3\r\n"
+  parseWith (getContents sock) >>= print
+  sendAll sock "PING\r\n"
+  parseWith (getContents sock) >>= print
+  sendAll sock (encode (RespArray [RespBulkString "SET", RespBulkString "key", RespBulkString "value"])) >>= print
+  parseWith (getContents sock) >>= print
+  sendAll sock (encode (RespArray [RespBulkString "GET", RespBulkString "key"])) >>= print
+  parseWith (getContents sock) >>= print
   -- Load system CA certificates
-  store <- getSystemCertificateStore
+  -- store <- getSystemCertificateStore
 
-  let clientParams =
-        (defaultParamsClient hostname "redis-server")
-          { clientSupported =
-              def
-                { supportedVersions = [TLS13, TLS12],
-                  supportedCiphers = ciphersuite_strong
-                },
-            clientShared =
-              def
-                { sharedCAStore = store
-                }
-          }
-  context <- contextNew sock clientParams
-  handshake context
-  print "Handshake done"
-  sendData context "HELLO 3 AUTH default <password>\r\n"
-  recvData context >>= print
-  sendData context "PING\r\n"
-  recvData context >>= print
-  bye context
+  -- let clientParams =
+  --       (defaultParamsClient hostname "redis-server")
+  --         { clientSupported =
+  --             def
+  --               { supportedVersions = [TLS13, TLS12],
+  --                 supportedCiphers = ciphersuite_strong
+  --               },
+  --           clientShared =
+  --             def
+  --               { sharedCAStore = store
+  --               }
+  --         }
+  -- context <- contextNew sock clientParams
+  -- handshake context
+  -- print "Handshake done"
+  -- sendData context "HELLO 3 AUTH default"
+  -- parseWith (recvData context) >>= print
+  -- sendData context "PING\r\n"
+  -- parseWith (recvData context) >>= print
+  -- bye context
   close sock
   return ()
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,11 +2,11 @@
 
 module Main where
 
-import Test.Hspec
 import Data.ByteString.Char8 qualified as B
-import Resp
 import Data.Map qualified as M
 import Data.Set qualified as S
+import Resp
+import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
@@ -35,7 +35,7 @@ main = hspec $ do
       let mapData = M.fromList [(RespSimpleString "first", RespInteger 1), (RespSimpleString "second", RespBulkString "bulkString")]
       encode (RespMap mapData) `shouldBe` "*2\r\n+first\r\n:1\r\n+second\r\n$10\r\nbulkString\r\n"
       encode (RespMap M.empty) `shouldBe` "*0\r\n"
-    
+
     it "encodes sets correctly" $ do
       let setData = S.fromList [(RespSimpleString "Hello"), (RespInteger 5), RespBulkString ("im very long")]
       encode (RespSet setData) `shouldBe` "~3\r\n+Hello\r\n:5\r\n$12\r\nim very long\r\n"
