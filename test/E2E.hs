ca5da3c92308e3d34005e5e61317002b4755445f
Seth Speaks <sspeaks610@gmail.com>
2025-02-21 16:36:47 -0800

    added pipelining e2e tests

--- test/E2E.hs
+++ test/E2E.hs
@@ -2,13 +2,19 @@
 
 module Main where
 
-import Client (PlainTextClient)
+import Client (Client (..), PlainTextClient)
+import Control.Monad.State qualified as State
+import Data.ByteString.Char8 qualified as BSC
 import RedisCommandClient
   ( RedisCommandClient,
     RedisCommands (..),
     runCommandsAgainstPlaintextHost,
   )
-import Resp (RespData (RespArray, RespBulkString, RespSimpleString))
+import Resp
+  ( Encodable (encode),
+    RespData (RespArray, RespBulkString, RespSimpleString),
+    parseManyWith,
+  )
 import Test.Hspec
 
 runRedisAction :: RedisCommandClient PlainTextClient a -> IO a
@@ -28,3 +34,21 @@ main = hspec $ do
       runRedisAction (get "a") `shouldReturn` RespBulkString "b"
       runRedisAction (get "c") `shouldReturn` RespBulkString "d"
       runRedisAction (get "e") `shouldReturn` RespBulkString "f"
+
+  describe "Pipelining works" $ do
+    it "can pipeline 100 commands" $ do
+      runRedisAction
+        ( do
+            client <- State.get
+            send client $ mconcat ([encode . RespArray $ map RespBulkString ["SET", "KEY" <> BSC.pack (show n), "VALUE" <> BSC.pack (show n)] | n <- [1 .. 100]])
+            parseManyWith 100 (recieve client)
+        )
+        `shouldReturn` replicate 100 (RespSimpleString "OK")
+    it "can retrieve 100 command results with pipelining" $ do
+      runRedisAction
+        ( do
+            client <- State.get
+            send client $ mconcat ([encode . RespArray $ map RespBulkString ["GET", "KEY" <> BSC.pack (show n)] | n <- [1 .. 100]])
+            parseManyWith 100 (recieve client)
+        )
+        `shouldReturn` [RespBulkString ("VALUE" <> BSC.pack (show n)) | n <- [1 .. 100]]
