d858da40ee66e7dac1660d08b35db5a083cc6621
Seth Speaks <sspeaks610@gmail.com>
2025-02-24 18:19:03 -0800

    experiment

--- app/Filler.hs
+++ app/Filler.hs
@@ -7,7 +7,7 @@ module Filler where
 import Client (Client (..), ConnectionStatus (Connected))
 import Control.Concurrent (MVar, ThreadId, forkIO, myThreadId, newEmptyMVar, putMVar, takeMVar, throwTo)
 import Control.Exception (IOException, catch)
-import Control.Monad (replicateM_)
+import Control.Monad (forM_, replicateM_)
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Control.Monad.State qualified as State
 import Data.Attoparsec.ByteString.Lazy qualified as Atto
@@ -25,10 +25,13 @@ import Text.Printf (printf)
 randomBytes :: (StatefulGen g m) => g -> Int -> m SB.ByteString
 randomBytes g b = uniformByteStringM b g
 
+sizeOfKeyVals :: Int
+sizeOfKeyVals = 512
+
 genRandomSet :: (StatefulGen g m) => g -> m LB.ByteString
 genRandomSet gen = do
-  bytesToSend <- LB.fromStrict <$> randomBytes gen (numKilosToPipeline * 1024) -- 1 GB of bytes
-  case Atto.parseOnly (Atto.count numKilosToPipeline parseSet) bytesToSend of
+  bytesToSend <- LB.fromStrict <$> randomBytes gen (numKeysToPipeline * sizeOfKeyVals) -- 1 GB of bytes
+  case Atto.parseOnly (Atto.count numKeysToPipeline parseSet) bytesToSend of
     Left err -> error err
     Right v -> return $ Builder.toLazyByteString $ mconcat v
   where
@@ -37,23 +40,38 @@ genRandomSet gen = do
     setBuilder = Builder.stringUtf8 "*3\r\n" <> (encode . RespBulkString $ "SET")
     parseSet :: Atto.Parser Builder.Builder
     parseSet = do
-      key <- encode . RespBulkString . LB.fromStrict <$> Atto.take 512
-      val <- encode . RespBulkString . LB.fromStrict <$> Atto.take 512
+      key <- encode . RespBulkString . LB.fromStrict <$> Atto.take (sizeOfKeyVals `div` 2)
+      val <- encode . RespBulkString . LB.fromStrict <$> Atto.take (sizeOfKeyVals `div` 2)
       return $ setBuilder <> key <> val
 
-numKilosToPipeline :: Int
-numKilosToPipeline = 1024 * 1024 -- 1 gigabyte
+genGigSetRandom :: Int -> LB.ByteString -> LB.ByteString
+genGigSetRandom start value =
+  let tl = encode $ RespBulkString value
+   in Builder.toLazyByteString $ go (1024 * 10) tl
+  where
+    setBuilder :: Builder.Builder
+    setBuilder = Builder.stringUtf8 "*3\r\n" <> (encode . RespBulkString $ "SET")
+    valBuilder :: Int -> Builder.Builder
+    valBuilder i = Builder.stringUtf8 "$" <> Builder.intDec (length $ show i) <> "\r\n" <> Builder.intDec i <> "\r\n"
+    go :: Int -> Builder.Builder -> Builder.Builder
+    go 0 _ = mempty
+    go !n tl = setBuilder <> valBuilder (n + start) <> tl <> go (n - 1) tl
+
+numKeysToPipeline :: Int
+numKeysToPipeline = (1024 * 1024 * 1024) `div` sizeOfKeyVals -- 1 gigabyte
 
 fillCacheWithData :: (Client client) => Int -> RedisCommandClient client ()
 fillCacheWithData gb = do
   client <- State.get
   seed <- liftIO $ round <$> getPOSIXTime
   gen <- newIOGenM (mkStdGen seed)
+  val <- LB.fromStrict <$> randomBytes gen (1024 * 100)
   doneMvar <- liftIO newEmptyMVar
   parentThread <- liftIO myThreadId
+  -- _ <- liftIO $ print $ genGigSetRandom 1 val
   _ <- liftIO $ forkIO (readerThread parentThread client gb doneMvar)
-  replicateM_ gb $ do
-    _ <- liftIO (genRandomSet gen) >>= send client
+  forM_ [1 .. gb] $ \iter -> do
+    _ <- send client (genGigSetRandom ((iter -1) * 1024 * 10) val)
     liftIO $ printf "+1GB written in fireAndForget mode\n"
   liftIO $ printf "Done writing... waiting on read thread to finish...\n"
   result <- liftIO $ takeMVar doneMvar
@@ -70,7 +88,7 @@ readerThread :: (Client client) => ThreadId -> client 'Connected -> Int -> MVar 
 readerThread parentThread client numGbToRead errorOrDone =
   ( do
       replicateM_ numGbToRead $ do
-        !res <- find isError <$> parseManyWith numKilosToPipeline (receive client)
+        !res <- find isError <$> parseManyWith (1024 * 10) (receive client)
         case extractError <$> res of
           Nothing -> return ()
           Just s -> fail ("error encountered from RESP values read from socket: " <> s)
