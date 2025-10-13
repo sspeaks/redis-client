4e3283067f54963964c426a3b2a926872c7b8a51
Seth Speaks <sspeaks610@gmail.com>
2025-10-13 15:09:16 -0700

    make script work out of box

--- rune2eTests.sh
+++ rune2eTests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # shellcheck disable=SC2046
+
+docker load <$(nix build "github:sspeaks/nixos-config#garnet-image" --print-out-paths)
 docker load <$(nix-build e2eDockerImg.nix)
 docker-compose up --exit-code-from e2etests
 docker-compose down
